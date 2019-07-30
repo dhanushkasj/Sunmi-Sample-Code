2eaf38849e1ba2fbe17d015a570a69f585f8969f
Dhanushka Jayakody <dhanushka@jgtsltd.co.uk>
2019-07-30 14:57:33 +0530

    Add project files.

---
+++ SunmiDemo/SunmiDemo/ViewModel/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace SunmiDemo.ViewModel
+{
+    public class MainViewModel
+    {
+        public ICommand PrintCommand { get; private set; }
+        public ICommand ScanCommand { get; private set; }
+        public ICommand LFCommand { get; private set; }
+
+        public MainViewModel()
+        {
+            PrintCommand = new Command(async () => await PrintItem());
+            ScanCommand = new Command(async () => await ScanItem());
+            LFCommand = new Command(async () => await PrinterCommand());
+        }
+
+        async Task ScanItem()
+        {
+            
+            
+            //var _navigation = Application.Current.MainPage.Navigation;
+            //var _lastPage = _navigation.NavigationStack.LastOrDefault();
+
+            //var NewPage = new cpgScan();
+            //await Navigation.PushAsync(NewPage);
+
+            //_navigation.RemovePage(_lastPage);
+        }
+
+        async Task PrinterCommand()
+        {
+            Xamarin.Forms.DependencyService.Register<INativePages>();
+            DependencyService.Get<INativePages>().StartActivityInAndroid("LF");
+        }
+
+        async Task PrintItem()
+        {
+            // await Application.Current.MainPage.Navigation.PushModalAsync(new cpgPrint());
+            Xamarin.Forms.DependencyService.Register<INativePages>();
+            DependencyService.Get<INativePages>().StartActivityInAndroid("Hello Sunmi,\nI am printing some text\n");
+        }
+
+    }
+}
